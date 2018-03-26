4bff96a0b1b71bec081727b22599b34290e7ab06
Lauren Hayward Sierens <lhayward@perimeterinstitute.ca>
2018-03-26 14:13:09 -0400

    Plotting adjustments

--- Tutorials/Tutorial2/Code/tutorial2_spirals_tensorflow.py
+++ Tutorials/Tutorial2/Code/tutorial2_spirals_tensorflow.py
@@ -4,17 +4,14 @@
 ###
 ######################################################################################
 
-# A bit of setup
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # figure setup
-plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
-#plt.rcParams['image.cmap'] = 'gray'
 
-# activation function
+### Activation function
 def sigmoid(x):
   return 1. / (1. + np.exp(-x))
 
@@ -27,21 +24,24 @@ K = 3 # number of branches
 X = np.zeros((N*K,D)) # matrix containing the dataset
 y = np.zeros(N*K, dtype='uint8') # labels
 
-mag_noise = 0.2 #0.2
-dTheta    = 4 #4
+mag_noise = 0.3 #0.2
+dTheta    = 5 #4
 # data generation
 for j in range(K):
   ix = range(N*j,N*(j+1))
-  r = np.linspace(0.0,1,N) # radius
+  r = np.linspace(0.01,1,N) # radius
   t = np.linspace(j*(2*np.pi)/K,j*(2*np.pi)/K + dTheta,N) + np.random.randn(N)*mag_noise # theta
   X[ix] = np.c_[r*np.cos(t), r*np.sin(t)]
   y[ix] = j
 
+print y
 # plotting dataset
-fig = plt.figure()
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40), #cmap=plt.cm.Spectral)
+fig = plt.figure(figsize=(6,6))
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40)#, cmap=plt.cm.Spectral)
 plt.xlim([-1,1])
 plt.ylim([-1,1])
+plt.xlabel('x')
+plt.ylabel('y')
 fig.savefig('spiral_raw.pdf')
 
 # Computational graph for tensorflow
@@ -49,7 +49,7 @@ fig.savefig('spiral_raw.pdf')
 x = tf.placeholder(tf.float32, [None, D])
 yl = tf.placeholder(tf.int32,[None])
 
-h = 100 # size of hidden layer
+h = 20 # size of hidden layer
 
 W1 = tf.Variable(tf.random_normal([ D, h], mean=0.0, stddev=0.01, dtype=tf.float32))
 b1 = tf.Variable(tf.zeros([h]))
@@ -64,11 +64,6 @@ a2 = tf.nn.sigmoid(z2)
 #cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=yl,logits=z2)) # tf shortcut 
 eps=0.0000000001
 cross_entropy = tf.reduce_mean(-tf.reduce_sum( tf.one_hot(yl,depth=K) * tf.log(a2+eps) +  (1.0-tf.one_hot(yl,depth=K) )*tf.log(1.0-a2 +eps) , reduction_indices=[1])) # a little more explicit
-
-# Shortest and least error-prone version using tensorflow layers
-#a1 = tf.layers.dense(inputs=x, units=h, activation=tf.nn.sigmoid)
-#a2 = tf.layers.dense(inputs=hidden_layer, units=K)
-#cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=yl,logits=scores))
 
 # some hyperparameters
 step_size = 1.0 #e-0
@@ -86,8 +81,6 @@ with tf.Session() as sess:
         if i % 1000 == 0:
             loss=sess.run(cross_entropy,feed_dict={x: X,yl:y})
             print "iteration %d: loss %f" % (i, loss)
-  
-
 
     #hidden_layer = np.maximum(0, np.dot(X, W) + b)
     #scores = np.dot(hidden_layer, W2) + b2
@@ -104,18 +97,21 @@ with tf.Session() as sess:
 
 # plot the resulting classifier
 h = 0.02
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+x_min, x_max = X[:, 0].min() - 0.25, X[:, 0].max() + 0.25
+y_min, y_max = X[:, 1].min() - 0.25, X[:, 1].max() + 0.25
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
 Z = np.dot(sigmoid( np.dot(np.c_[xx.ravel(), yy.ravel()], W1) + b1), W2) + b2
 Z = np.argmax(Z, axis=1)
 Z = Z.reshape(xx.shape)
-fig = plt.figure()
-plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+
+fig = plt.figure(figsize=(6,6))
+plt.contourf(xx, yy, Z, K, alpha=0.8)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40)
 plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
+plt.xlabel('x')
+plt.ylabel('y')
 fig.savefig('spiral_net_results.pdf')
 
 plt.show()
